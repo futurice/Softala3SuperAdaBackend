07064beb3cdb943827614a87e3eddd2882719813
Rasmus Eskola <fruitiex@gmail.com>
2017-02-08 09:33:50 +0200

    Use SSL by default when connecting to remote databases

--- src/db.js
+++ src/db.js
@@ -1,11 +1,15 @@
 'use strict';
 
+var pg = require('pg');
+pg.defaults.ssl = true;
+
 exports.knexlocal = require('knex')({
   client: 'pg',
   connection: process.env.DATABASE_URL || {
     host : '127.0.0.1',
     user : 'postgres', /* whoami */
     password : '',
-    database : 'superada'
+    database : 'superada',
+    ssl: false
   }
 });
